77e3abd5f102733f7030b534296048ab325c5ed3
Thiévent Florian (IT-PTR-CEN2-BDE7) <florian.thievent@sbb.ch>
2023-10-03 14:56:40 +0200

    changed

--- flind_core/settings.py
+++ flind_core/settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from celery.schedules import crontab
 import os
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
