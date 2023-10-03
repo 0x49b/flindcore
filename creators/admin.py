502bbefa65b2580d8533983342c6bdcc944bdc92
Thiévent Florian (IT-PTR-CEN2-BDE7) <florian.thievent@sbb.ch>
2023-10-03 15:43:07 +0200

    added sb url

--- creators/admin.py
+++ creators/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Creator, CreatorMeta
+
 
 # Register your models here.
+@admin.register(Creator)
+class CreatorAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Creator._meta.get_fields()]
+
+
+@admin.register(CreatorMeta)
+class CreatorMetaAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in CreatorMeta._meta.get_fields()]
