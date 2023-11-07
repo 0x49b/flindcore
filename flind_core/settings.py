220e957a4f235cf99e5409e3fe6fb2e9fd36063c
Thiévent Florian (IT-PTR-CEN2-BDE7) <florian.thievent@sbb.ch>
2023-11-07 11:17:09 +0100

    changed settings

--- flind_core/settings.py
+++ flind_core/settings.py
@@ -78,9 +78,9 @@ if IS_HEROKU_APP:
     # https://github.com/jazzband/dj-database-url
     DATABASES = {
         "default": dj_database_url.config(
-            conn_max_age=600,
+            conn_max_age=0,
             conn_health_checks=True,
-            ssl_require=True,
+            ssl_require=False,
         ),
     }
 else:
